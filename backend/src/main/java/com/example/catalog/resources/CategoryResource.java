47b056cf1bf5446605413cc8f55791116595ac8b
Joao Vitor Nantes <joaohn1@hotmail.com>
2023-12-27 14:22:41 -0300

    H2 database

--- backend/src/main/java/com/example/catalog/resources/CategoryResource.java
+++ backend/src/main/java/com/example/catalog/resources/CategoryResource.java
@@ -1,6 +1,8 @@
 package com.example.catalog.resources;
 
 import com.example.catalog.entities.Category;
+import com.example.catalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +14,12 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
+
+    @Autowired
+    private CategoryService service;
     @GetMapping
     public ResponseEntity<List<Category>> findAll(){
-        List<Category> list = new ArrayList<>();
-        list.add(new Category(1L, "Books"));
-        list.add(new Category(2L, "Livros"));
+        List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
