de81f1134df12609e1b9d671fe05067619d6887a
Joao Vitor Nantes <joaohn1@hotmail.com>
2023-12-27 16:26:23 -0300

    database seeding

--- backend/src/main/java/com/example/catalog/services/CategoryService.java
+++ backend/src/main/java/com/example/catalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.example.catalog.entities.Category;
 import com.example.catalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ public class CategoryService {
 
     @Autowired
     private CategoryRepository repository;
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
         return repository.findAll();
     }
