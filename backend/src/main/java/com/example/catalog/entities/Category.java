4c919ae5f0511df87ffa69d56bacf27f4610454c
Joao Vitor Nantes <joaohn1@hotmail.com>
2023-12-28 14:48:50 -0300

    Delete category and Auditing data

--- backend/src/main/java/com/example/catalog/entities/Category.java
+++ backend/src/main/java/com/example/catalog/entities/Category.java
@@ -3,6 +3,7 @@ package com.example.catalog.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 @Entity
 @Table(name = "tb_category")
@@ -11,8 +12,11 @@ public class Category implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updateAt;
 
     public Category(){}
 
@@ -37,6 +41,21 @@ public class Category implements Serializable {
         this.name = name;
     }
 
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdateAt() {
+        return updateAt;
+    }
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+    @PreUpdate
+    public void preUpdate() {
+        updateAt = Instant.now();
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- backend/src/main/java/com/example/catalog/services/CategoryService.java
+++ backend/src/main/java/com/example/catalog/services/CategoryService.java
@@ -3,9 +3,12 @@ package com.example.catalog.services;
 import com.example.catalog.dto.CategoryDTO;
 import com.example.catalog.entities.Category;
 import com.example.catalog.repositories.CategoryRepository;
+import com.example.catalog.services.exceptions.DatabaseException;
 import com.example.catalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -48,6 +51,18 @@ public class CategoryService {
         }
         catch(EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
         }
     }
 }
