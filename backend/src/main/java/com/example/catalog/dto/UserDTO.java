fabe664203bd1e9fef574a94617a181b6d8b6243
Joao Vitor Nantes <joaohn1@hotmail.com>
2024-01-04 15:42:51 -0300

    Basic beans validation

--- backend/src/main/java/com/example/catalog/dto/UserDTO.java
+++ backend/src/main/java/com/example/catalog/dto/UserDTO.java
@@ -9,14 +9,16 @@ import java.util.Set;
 import com.example.catalog.entities.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 
 public class UserDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
-
+    @NotBlank(message = "Campo obrigatório")
     private String firstName;
     private String lastName;
+    @Email(message = "Favor entrar um e-mail válido")
     private String email;
 
     Set<RoleDTO> roles = new HashSet<>();
--- backend/src/main/java/com/example/catalog/resources/UserResource.java
+++ backend/src/main/java/com/example/catalog/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.example.catalog.resources;
 
 import java.net.URI;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -40,7 +41,7 @@ public class UserResource {
     }
 
     @PostMapping
-    public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto) {
+    public ResponseEntity<UserDTO> insert(@Valid @RequestBody UserInsertDTO dto) {
         UserDTO newDto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(newDto.getId()).toUri();
@@ -48,7 +49,7 @@ public class UserResource {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<UserDTO> update(@PathVariable Long id,@RequestBody UserDTO dto) {
+    public ResponseEntity<UserDTO> update(@PathVariable Long id,@Valid @RequestBody UserDTO dto) {
         UserDTO newDto = service.update(id, dto);
         return ResponseEntity.ok().body(newDto);
     }
